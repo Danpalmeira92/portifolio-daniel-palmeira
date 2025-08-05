eb164a9a70e261e76577ad180a181d5888623e39
Danpalmeira92 <danpalmeira1992@gmail.com>
2025-08-04 21:18:37 -0300

    Estilizando a sidebar

--- src/containers/Sidebar/index.tsx
+++ src/containers/Sidebar/index.tsx
@@ -1,9 +1,22 @@
+import Avatar from '../../components/Avatar'
+import Paragrafo from '../../components/Paragrafo'
 import Titulo from '../../components/Titulo'
+
+import { BotaoTema, Descricao, SidebarContainer } from './styles'
 
 const Sidebar = () => (
   <aside>
-    <img src="https://github.com/Danpalmeira92.png" alt="" />
-    <Titulo fontSize={20}>Daniel Palmeira</Titulo>
+    <SidebarContainer>
+      <Avatar />
+      <Titulo fontSize={16}>Daniel Palmeira</Titulo>
+      <Paragrafo tipo="secundario" font-size={16}>
+        Danpalmeira92
+      </Paragrafo>
+      <Descricao tipo="principal" fontSize={10}>
+        Desenvolvedor Front-end
+      </Descricao>
+      <BotaoTema>Trocar tema</BotaoTema>
+    </SidebarContainer>
   </aside>
 )
 
